4489a58635164deba31654932014cc500120157e
Johnathan <jocuog@gmail.com>
2022-05-17 21:54:05 -0600

    commit tree e0e3e9207745b396dcd1e7b82446142e19ddbb2b

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validates :content, presence: true, length: { minimum: 100 }
+
 end
